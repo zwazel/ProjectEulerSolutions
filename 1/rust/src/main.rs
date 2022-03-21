22bbe6b88b4d5ae8dc1e6cb7897e8de871588dfa
Zwazel <zwazel@hotmail.com>
2022-03-21 08:24:40 +0100

    less code

--- 1/rust/src/main.rs
+++ 1/rust/src/main.rs
@@ -2,11 +2,7 @@ fn main() {
     let mut sum = 0;
 
     for i in 1..1000 {
-        if i % 3 == 0 {
-            println!("{}", i);
-            sum += i;
-        } else if i % 5 == 0 {
-            println!("{}", i);
+        if i % 3 == 0 || i % 5 == 0 {
             sum += i;
         }
     }
