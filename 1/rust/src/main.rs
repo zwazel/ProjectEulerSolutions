e7b88350811d402f3316a5bc42c12716be1524e3
Zwazel <zwazel@hotmail.com>
2022-03-21 08:23:06 +0100

    done

--- 1/rust/src/main.rs
+++ 1/rust/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum = 0;
+
+    for i in 1..1000 {
+        if i % 3 == 0 {
+            println!("{}", i);
+            sum += i;
+        } else if i % 5 == 0 {
+            println!("{}", i);
+            sum += i;
+        }
+    }
+
+    println!("Sum: {}", sum);
 }
