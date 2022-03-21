0849ad23bd0e1a7bc56e9823df65f0e7e9faf7b6
Zwazel <zwazel@hotmail.com>
2022-03-21 09:05:08 +0100

    idk

--- 2/rust/src/main.rs
+++ 2/rust/src/main.rs
@@ -1,15 +1,22 @@
 fn main() {
-    let mut sum = 0;
-
-    let mut num1 = 1;
-    let mut num2 = 2;
+    let mut sum = 2;
+    let mut numbers = vec![1, 2];
     let mut num = 0;
-    while num < 100 {
-        num = num1 + num2;
-        num1 = num2;
-        num2 = num;
+    while num < 4_000_000 {
+        let num1 = numbers.get(numbers.len() - 2);
+        let num2 = numbers.get(numbers.len() - 1);
+        if num1.is_none() || num2.is_none() {
+            eprintln!("Error: numbers.get(numbers.len() - 2) or numbers.get(numbers.len() - 1) is None");
+            break;
+        } else {
+            num = num1.unwrap() + num2.unwrap();
+            numbers.push(num);
 
-        sum += num;
-        println!("num: {}", num);
+            if num & 2 == 0 {
+                sum += num;
+            }
+            println!("num: {}", num);
+        }
     }
+    println!("sum: {}", sum);
 }
