983a70d8327d4243b5741524792e92817607214b
Zwazel <zwazel@hotmail.com>
2022-03-28 09:13:14 +0200

    i am getting palindrome but not really the one i want

--- 4/rust/src/main.rs
+++ 4/rust/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let mut biggest_palindrome = 0;
+    for i in 10..99 {
+        for j in 10..99 {
+            let num = i * j;
+            let mut digits = num.to_string().chars().collect::<Vec<char>>();
+            if digits.len() % 2 == 0 {
+                let mut same = true;
+                let len = digits.len();
+                for k in 0..len / 2 {
+                    if digits[k] == digits[digits.len() - 1 - k] {
+                        continue;
+                    } else {
+                        same = false;
+                        break;
+                    }
+                }
+                if same {
+                    if num > biggest_palindrome {
+                        biggest_palindrome = num;
+                        println!("{}", num);
+                    }
+                }
+            }
+        }
+    }
 }
