79704d97b7c3984ec9960376864f4a6ba8677219
Zwazel <zwazel@hotmail.com>
2022-03-28 10:28:51 +0200

    easy

--- 6/rust/src/main.rs
+++ 6/rust/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let num = 100;
+    let square_of_sum = square_of_sum(num);
+    let sum_of_squares = sum_of_squares(num);
+
+    println!("{}", square_of_sum - sum_of_squares);
+}
+
+fn square_of_sum(n: i32) -> i32 {
+    let sum: i32 = (1..n + 1).sum();
+    sum * sum
+}
+
+fn sum_of_squares(n: i32) -> i32 {
+    let mut sum = 0;
+    for i in 1..n + 1 {
+        sum += i * i;
+    }
+    sum
 }
