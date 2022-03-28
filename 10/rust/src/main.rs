24c74d52667be6543d83d194cb16afd3c201dafb
Zwazel <zwazel@hotmail.com>
2022-03-28 17:03:23 +0200

    brute forcing = slow

--- 10/rust/src/main.rs
+++ 10/rust/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let max_searches = 2_000_000;
+    let mut sum: i64 = 0;
+
+    for i in 0..max_searches {
+        if is_prime(i) {
+            sum += i as i64;
+        }
+    }
+
+    println!("Sum of primes below {} is {}", max_searches, sum);
+}
+
+fn is_prime(n: i32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
 }
