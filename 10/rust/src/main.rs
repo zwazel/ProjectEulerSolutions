0b6d4cf1b883c7b44c340d2bddc144065af54067
Zwazel <zwazel@hotmail.com>
2022-03-28 18:21:23 +0200

    i did it

--- 10/rust/src/main.rs
+++ 10/rust/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    // let max_searches = 2_000_000;
-    let max_searches = 10;
-    let mut sum: i64 = 0;
+    let max_searches = 2_000_000;
+    let mut sum: i128 = 0;
 
     let mut primes: Vec<bool> = vec![false, false];
-    for _i in 2..max_searches {
+    for i in 2..max_searches {
         primes.push(true);
     }
 
@@ -12,26 +11,24 @@ fn main() {
 
     for i in 0..primes.len() {
         if primes[i] {
-            sum += i as i64;
+            sum += i as i128;
         }
     }
 
     println!("Sum of primes below {} is {}", max_searches, sum);
 }
 
-fn cross_out(p: usize, primes: &mut Vec<bool>) {
-    let mut i = p * p;
+fn cross_out(mut p: usize, primes: &mut Vec<bool>) {
+    while p < primes.len() {
+        let mut i = p * p;
+        while i < primes.len() {
+            primes[i] = false;
 
-    while i < primes.len() {
-        primes[i] = false;
-
-        i += p;
-        if i > primes.len() {
-            break;
+            i += p;
+            if i > primes.len() {
+                break;
+            }
         }
-    }
-
-    if p < primes.len() {
-        cross_out(p + 1, primes);
+        p += 1;
     }
 }
