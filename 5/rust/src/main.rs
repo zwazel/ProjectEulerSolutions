3e21987d1b9b2bcd24d49029b9554022aea82114
Zwazel <zwazel@hotmail.com>
2022-03-28 10:22:01 +0200

    getting the test number

--- 5/rust/src/main.rs
+++ 5/rust/src/main.rs
@@ -1,8 +1,24 @@
 fn main() {
     let min = 2;
     let max = 10;
+    let mut done = false;
 
-    for i in min..max + 1 {
-        
+    let mut num = max;
+    while !done {
+        let mut good = true;
+        for i in min..max + 1 {
+            if num % i != 0 {
+                good = false;
+                break;
+            }
+        }
+
+        if good {
+            done = true;
+        } else {
+            num += 1;
+        }
     }
+
+    println!("{}", num);
 }
