6e00016189d73d16b28485f440b2c738f08a56b8
Zwazel <zwazel@hotmail.com>
2022-03-28 07:51:10 +0200

    trying to get those f*ing prime numbers

--- 3/rust/src/main.rs
+++ 3/rust/src/main.rs
@@ -19,11 +19,25 @@ fn get_prime_factors(num: i32) -> Vec<i32> {
         1
     };
 
-    let mut muliply2 = num / multiply1;
+    let mut multiply2 = num / multiply1;
 
-    println!("multiply1: {}\nmultiply2: {}", multiply1, muliply2);
+    println!("multiply1: {}\nmultiply2: {}", multiply1, multiply2);
 
+    if (multiply1 * multiply2) == num {
+        println!("Prime numbers equal to {} if multiplied", num);
+    } else {
+        println!("Prime numbers do not equal to {} if multiplied, they equal to {}", num, multiply1 * multiply2);
+    }
 
+    if is_prime(multiply1) {
+        prime_factors.push(multiply1);
+    } else {
+        
+    }
+
+    if is_prime(multiply2) {
+        prime_factors.push(multiply2);
+    }
 
     prime_factors.sort();
     prime_factors.reverse();
