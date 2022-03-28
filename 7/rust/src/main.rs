f28ffef29d712d7f657941c79203585aab5b71e6
Zwazel <zwazel@hotmail.com>
2022-03-28 10:50:31 +0200

    done

--- 7/rust/src/main.rs
+++ 7/rust/src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    let prime_num_to_find = 10001;
+    let mut prime_count = 0;
+    let mut num = 2;
+    loop {
+        if prime_count >= prime_num_to_find {
+            break;
+        } else {
+            if is_prime(num) {
+                prime_count += 1;
+                if prime_count == prime_num_to_find {
+                    break;
+                }
+            }
+
+            num += 1;
+        }
+    }
+    println!("{}", num);
+}
+
+fn is_prime(num: i32) -> bool {
+    if num == 2 {
+        return true;
+    }
+    if num % 2 == 0 {
+        return false;
+    }
+    let mut i = 3;
+    while i * i <= num {
+        if num % i == 0 {
+            return false;
+        }
+        i += 2;
+    }
+    return true;
 }
