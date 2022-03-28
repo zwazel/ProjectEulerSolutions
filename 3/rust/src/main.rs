620f8591a10e03e912a74095b8dede178ffe21e3
Zwazel <zwazel@hotmail.com>
2022-03-28 08:53:51 +0200

    oke cool i did it

--- 3/rust/src/main.rs
+++ 3/rust/src/main.rs
@@ -1,59 +1,26 @@
 fn main() {
-    let num = 13195;
+    let mut num: i64 = 600851475143;
 
-    let prime_factors = get_prime_factors(num);
+    let mut prime_factors: Vec<i64> = Vec::new();
 
-    println!("{:?}", prime_factors);
-}
-
-fn get_prime_factors(num: i32) -> Vec<i32> {
-    let mut prime_factors: Vec<i32> = vec![];
-
-    let mut multiply1 = if num % 10 == 0 {
-        num / 10
-    } else if num % 5 == 0 {
-        num / 5
-    } else if num % 2 == 0 {
-        num / 2
-    } else {
-        1
-    };
-
-    let mut multiply2 = num / multiply1;
-
-    println!("multiply1: {}\nmultiply2: {}", multiply1, multiply2);
-
-    if (multiply1 * multiply2) == num {
-        println!("Prime numbers equal to {} if multiplied", num);
-    } else {
-        println!("Prime numbers do not equal to {} if multiplied, they equal to {}", num, multiply1 * multiply2);
+    while num % 2 == 0 {
+        prime_factors.push(2);
+        num /= 2;
     }
 
-    if is_prime(multiply1) {
-        prime_factors.push(multiply1);
-    } else {
-        
+    for i in 3..(num as f64).sqrt() as i64 {
+        while num % i == 0 {
+            prime_factors.push(i);
+            num /= i;
+        }
     }
 
-    if is_prime(multiply2) {
-        prime_factors.push(multiply2);
+    if num > 2 {
+        prime_factors.push(num);
     }
 
     prime_factors.sort();
     prime_factors.reverse();
 
-    prime_factors
-}
-
-fn is_prime(num: i32) -> bool {
-    let mut is_prime = true;
-
-    for i in 2..num {
-        if num % i == 0 {
-            is_prime = false;
-            break;
-        }
-    }
-
-    is_prime
+    println!("{:?}", prime_factors);
 }
