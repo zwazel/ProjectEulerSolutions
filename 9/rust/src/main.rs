1fb8d650bb5e41ddc5964e90e3e0a2f2fc2019da
Zwazel <zwazel@hotmail.com>
2022-03-28 16:45:00 +0200

    done

--- 9/rust/src/main.rs
+++ 9/rust/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let s = 1000;
+
+    for a in 1..s {
+        for b in a..s {
+            let c = s - a - b;
+            if a * a + b * b == c * c {
+                println!("{}", a * b * c);
+                return;
+            }
+        }
+    }
 }
