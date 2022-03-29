667f6be0650833317a14f592da1cb7c68c9fd59d
Zwazel <zwazel@hotmail.com>
2022-03-29 16:13:39 +0200

    now measuring time for num 10 for 2 different ways

--- 10/rust/src/main.rs
+++ 10/rust/src/main.rs
@@ -1,4 +1,7 @@
+use std::time::Instant;
+
 fn main() {
+    let start = Instant::now();
     let max_searches = 2_000_000;
     let mut sum: i128 = 0;
 
@@ -15,7 +18,44 @@ fn main() {
         }
     }
 
+
     println!("Sum of primes below {} is {}", max_searches, sum);
+    let elapsed = start.elapsed();
+    println!("Millis: {} ms", elapsed.as_millis());
+
+    println!("Doing it the other way:");
+    let start = Instant::now();
+
+    sum = 0;
+    for i in 0..max_searches {
+        if is_prime(i) {
+            sum += i as i128;
+        }
+    }
+
+    println!("Sum of primes below {} is {}", max_searches, sum);
+    let elapsed = start.elapsed();
+    println!("Millis: {} ms", elapsed.as_millis());
+}
+
+fn is_prime(num: i64) -> bool {
+    if num < 2 {
+        return false;
+    }
+
+    if num == 2 {
+        return true;
+    }
+
+    let mut i = 3;
+    while i * i <= num {
+        if num % i == 0 {
+            return false;
+        }
+        i += 2;
+    }
+
+    return true;
 }
 
 fn cross_out(mut p: usize, primes: &mut Vec<bool>) {
