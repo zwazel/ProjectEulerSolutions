768777d8b83b69e0943a7436c6f226ee298f13b2
Zwazel <zwazel@hotmail.com>
2022-03-31 11:14:20 +0200

    getting first num with 6 divisors

--- 12/rust/src/main.rs
+++ 12/rust/src/main.rs
@@ -1,19 +1,30 @@
 fn main() {
-    let triangle_number_to_find = 7; // 7 = the 7th number
+    let amount_of_divisors = 6;
+    let mut num = 0;
 
-    let mut sum = 0;
-    for i in 1..triangle_number_to_find + 1 {
-        sum += i;
-    }
-
-    let mut divisors = vec![];
-    for i in 1..sum + 1 {
-        if is_a_divisor(sum, i) {
-            divisors.push(i);
+    let mut current_triangle_counter = 1;
+    loop {
+        let mut sum = 0;
+        for i in 1..current_triangle_counter + 1 {
+            sum += i;
         }
+
+        let mut divisors = vec![];
+        for i in 1..sum + 1 {
+            if is_a_divisor(sum, i) {
+                divisors.push(i);
+            }
+        }
+
+        if divisors.len() >= amount_of_divisors {
+            num = sum;
+            break;
+        }
+
+        current_triangle_counter += 1;
     }
 
-    println!("{}: {:?}", sum, divisors);
+    println!("{}", num);
 }
 
 fn is_a_divisor(num: i32, divisor: i32) -> bool {
