51e3ca1b518f6c1ed91aee9e92767add93d076ae
Zwazel <zwazel@hotmail.com>
2022-03-31 11:10:10 +0200

    getting the problem stuff

--- 12/rust/src/main.rs
+++ 12/rust/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let triangle_number_to_find = 7; // 7 = the 7th number
+
+    let mut sum = 0;
+    for i in 1..triangle_number_to_find + 1 {
+        sum += i;
+    }
+
+    let mut divisors = vec![];
+    for i in 1..sum + 1 {
+        if is_a_divisor(sum, i) {
+            divisors.push(i);
+        }
+    }
+
+    println!("{}: {:?}", sum, divisors);
+}
+
+fn is_a_divisor(num: i32, divisor: i32) -> bool {
+    num % divisor == 0
 }
