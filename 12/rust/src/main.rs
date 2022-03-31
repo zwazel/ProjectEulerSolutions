f6a3ef9e6609d86eaab8bca6bfd731399a592df7
Zwazel <zwazel@hotmail.com>
2022-03-31 13:36:19 +0200

    working on 12

--- 12/rust/src/main.rs
+++ 12/rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug, Formatter};
+
 fn main() {
     let amount_of_divisors = 500;
     let mut num = 0;
@@ -31,4 +33,81 @@ fn main() {
 
 fn is_a_divisor(num: i32, divisor: i32) -> bool {
     num % divisor == 0
+}
+
+fn prime_factorization(mut num: i64) {
+    let mut prime_factors: Vec<i64> = Vec::new();
+
+    while num % 2 == 0 {
+        prime_factors.push(2);
+        num /= 2;
+    }
+
+    for i in 3..(num as f64).sqrt() as i64 + 1 {
+        while num % i == 0 {
+            prime_factors.push(i);
+            num /= i;
+        }
+    }
+
+    if num > 2 {
+        prime_factors.push(num);
+    }
+
+    let mut prime_factors_factors: Vec<PrimeFactor> = Vec::new();
+    for i in 0..prime_factors.len() {
+        let prime_num = prime_factors[i];
+
+        if prime_factors_factors.len() <= 0 {
+            let prime_factor = PrimeFactor {
+                num: prime_num,
+                factor: 1,
+            };
+            prime_factors_factors.push(prime_factor);
+            continue;
+        }
+
+        for j in 0..prime_factors_factors.len() {
+            let mut prime_factor_option = prime_factors_factors.get_mut(j);
+
+            match prime_factor_option {
+                None => {}
+                Some(ref mut prime_factor) => {
+                    if prime_factor.num == prime_num {
+                        prime_factor.set_factor(prime_factor.factor + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{:?}", prime_factors_factors);
+}
+
+struct PrimeFactor {
+    num: i64,
+    factor: i64,
+}
+
+impl PrimeFactor {
+    fn set_num(&mut self, num: i64) {
+        self.num = num;
+    }
+
+    fn set_factor(&mut self, factor: i64) {
+        self.factor = factor;
+    }
+
+    fn increase_factor(&mut self) {
+        self.factor += 1;
+    }
+}
+
+impl Debug for PrimeFactor {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("PrimeFactor")
+            .field("num", &self.num)
+            .field("factor", &self.factor)
+            .finish()
+    }
 }
