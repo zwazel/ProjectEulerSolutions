0b270fc015250a6a8cec8591b06ddfafab9d815f
Zwazel <zwazel@hotmail.com>
2022-04-01 11:21:03 +0200

    grepper

--- 13/rust/src/main.rs
+++ 13/rust/src/main.rs
@@ -106,12 +106,14 @@ fn main() {
         "53503534226472524250874054075591789781264330331690",
     ];
 
+    //grepper rust create bigint from string
     let mut vec_numbers: Vec<BigInt> = vec![];
 
     for i in 0..vec_number_string.len() {
         let bigint = BigInt::parse_bytes(vec_number_string[i].as_ref(), 10);
         vec_numbers.push(bigint.unwrap());
     }
+    //grepper end
 
     let sum = BigInt::sum(vec_numbers.iter());
     let sum_string = sum.to_string();
